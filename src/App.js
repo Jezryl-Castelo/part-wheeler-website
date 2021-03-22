443c1fc7af4da61659772645db5ce407641d30f2
Jezryl <yamabags@gmail.com>
2021-03-22 11:04:28 -0600

    add redirect

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import Nav from './components/nav/nav.component';
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
-import ContactPage from './pages/contact-page/contact-page.component';
+// import ContactPage from './pages/contact-page/contact-page.component';
 import Footer from "./components/footer/footer.component";
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Contact from './components/contact/contact.component';
 
 
 function App() {
@@ -20,7 +21,7 @@ function App() {
       exact>
           <Homepage />
         </Route>
-        <Route path="/contact" exact component={ContactPage}>
+        <Route path="/contact" exact component={Contact}>
           {/* <ContactPage />      */}
         </Route> 
       </Switch>
